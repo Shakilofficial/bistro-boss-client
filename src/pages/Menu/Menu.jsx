2bc51e4e756bf92bfa81b580e92757e5f7899d31
Md Shakil Hossain <mrshakilhossainofficial@gmail.com>
2024-03-28 13:18:14 +0600

    render all category item in our menu page

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,8 +1,21 @@
 import { Helmet } from "react-helmet-async";
 import menuImg from "../../assets/menu/banner3.jpg";
+import dessertImg from "../../assets/menu/dessert-bg.jpeg";
+import pizzaImg from "../../assets/menu/pizza-bg.jpg";
+import saladImg from "../../assets/menu/salad-bg.jpg";
+import soupImg from "../../assets/menu/soup-bg.jpg";
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
 import Cover from "../../components/Shared/Cover/Cover";
+import UseMenu from "../../hooks/useMenu";
+import MenuCategory from "./MenuCategory/MenuCategory";
 const Menu = () => {
+  const [menu] = UseMenu();
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const salad = menu.filter((item) => item.category === "salad");
+  const soup = menu.filter((item) => item.category === "soup");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => item.category === "offered");
+
   return (
     <div>
       <Helmet>
@@ -17,7 +30,22 @@ const Menu = () => {
         subHeading="Do not Miss"
         heading="Todays Offer"
       ></SectionTitle>
-      <h2>This is Menu Page</h2>
+      {/* Offered Menu Item */}
+      <MenuCategory items={offered}></MenuCategory>
+      {/* Desert Menu Item */}
+      <MenuCategory
+        items={dessert}
+        title="desserts"
+        img={dessertImg}
+      ></MenuCategory>
+      {/* Pizza */}
+      <MenuCategory items={pizza} title={"pizza"} img={pizzaImg}></MenuCategory>
+
+      {/* Salad*/}
+      <MenuCategory items={salad} title={"salad"} img={saladImg}></MenuCategory>
+
+      {/* Soup */}
+      <MenuCategory items={soup} title={"soup"} img={soupImg}></MenuCategory>
     </div>
   );
 };
---
+++ src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+import MenuItem from "../../../components/MenuItem/MenuItem";
+import Cover from "../../../components/Shared/Cover/Cover";
+
+const MenuCategory = ({ items, title, img }) => {
+  return (
+    <div className="pt-20 my-5">
+      {title && (
+        <Cover
+          img={img}
+          title={title}
+          subtitle="Would you like to try a dish?"
+        />
+      )}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 p-4 mt-16">
+        {items.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+      <div className="flex justify-center items-center">
+        <Link to={`/order/${title}`}>
+          <button className="btn btn-sm border-0 border-b-2 btn-outline btn-warning">
+            Order Now
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default MenuCategory;
