21b229260897c6a54ca405b547fc2c2a30a7f8ea
Md Shakil Hossain <mrshakilhossainofficial@gmail.com>
2024-03-29 03:40:48 +0600

    firebase setup with login and Signup page authntication

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,21 +1,55 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { FaFacebookF, FaGithub, FaGoogle } from "react-icons/fa6";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import {
-    LoadCanvasTemplate,
-    loadCaptchaEnginge,
-    validateCaptcha,
+  LoadCanvasTemplate,
+  loadCaptchaEnginge,
+  validateCaptcha,
 } from "react-simple-captcha";
+import Swal from "sweetalert2";
 import loginBg from "../../assets/others/authentication.png";
 import loginImg from "../../assets/others/authentication2.png";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Login = () => {
   const [disabled, setDisabled] = useState(true);
+  const { signIn } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const from = location.state?.from?.pathname || "/";
 
   useEffect(() => {
     loadCaptchaEnginge(6);
   }, []);
+
+  const [passwordError, setPasswordError] = useState("");
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    // Password validation
+    if (password.length < 8) {
+      setPasswordError("Password must be at least 8 characters long.");
+      return;
+    }
+    setPasswordError("");
+    signIn(email, password).then((result) => {
+      const user = result.user;
+      console.log(user);
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Log In Successfully",
+        showConfirmButton: false,
+        timer: 1000,
+      });
+      navigate(from, { replace: true });
+    });
+  };
 
   const handleValidateCaptcha = (e) => {
     const user_captcha_value = e.target.value;
@@ -37,7 +71,7 @@ const Login = () => {
           <div className="w-full md:w-1/2 py-16 md:ml-6">
             <h3 className="text-center font-bold text-4xl">Login</h3>
             <div>
-              <form className="py-10 mx-12">
+              <form onSubmit={handleLogin} className="py-10 mx-12">
                 <div className="mb-2">
                   <label className="text-lg font-semibold mb-2">Email</label>
                   <input
@@ -75,14 +109,17 @@ const Login = () => {
                   type="submit"
                   value="Log in"
                 />
-
-                <div className="text-red-500 text-center text-sm mt-1"></div>
+                {passwordError && (
+                  <div className="text-red-500 text-center text-sm mt-1">
+                    {passwordError}
+                  </div>
+                )}
               </form>
               <div className="">
                 <div className="font-semibold text-slate-500 text-center">
                   Do not have an account?
                   <Link
-                    to="/signUp"
+                    to="/signup"
                     className="ml-2 text-[#b06a00] hover:underline hover:underline-offset-4"
                   >
                     SignUp
---
+++ src/pages/Signup/Signup.jsx
@@ -0,0 +1,178 @@
+import { useContext } from "react";
+import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { FaFacebookF, FaGithub, FaGoogle } from "react-icons/fa6";
+import { Link, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import loginBg from "../../assets/others/authentication.png";
+import loginImg from "../../assets/others/authentication2.png";
+import { AuthContext } from "../../providers/AuthProvider";
+
+const Signup = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const { createUser, updateUserProfile } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    createUser(data.email, data.password).then((result) => {
+      const loggedUser = result.user;
+      console.log(loggedUser);
+      updateUserProfile(data.name, data.photoURL)
+        .then(() => {
+          console.log("user profile info updated");
+          reset();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "User created successfully.",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          navigate("/");
+        })
+        .catch((error) => console.log(error));
+    });
+  };
+  return (
+    <>
+      <Helmet>
+        <title>Bistro Boss | Sign Up</title>
+      </Helmet>
+
+      <div className="my-10" style={{ backgroundImage: `url(${loginBg})` }}>
+        <div className="px-4 max-w-7xl mx-auto flex flex-col md:flex-row-reverse gap-6 justify-center items-center">
+          <div className="w-full md:w-1/2 mx-auto">
+            <img src={loginImg} alt="" />
+          </div>
+          <div className="w-full md:w-1/2 py-16 md:ml-6">
+            <h3 className="text-center font-bold text-4xl">Sign Up</h3>
+            <div>
+              <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Name</span>
+                  </label>
+                  <input
+                    type="text"
+                    {...register("name", { required: true })}
+                    name="name"
+                    placeholder="Name"
+                    className="input input-bordered"
+                  />
+                  {errors.name && (
+                    <span className="text-red-600">Name is required</span>
+                  )}
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Photo URL</span>
+                  </label>
+                  <input
+                    type="text"
+                    {...register("photoURL", { required: true })}
+                    placeholder="Photo URL"
+                    className="input input-bordered"
+                  />
+                  {errors.photoURL && (
+                    <span className="text-red-600">Photo URL is required</span>
+                  )}
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    {...register("email", { required: true })}
+                    name="email"
+                    placeholder="Enter Email"
+                    className="input input-bordered"
+                  />
+                  {errors.email && (
+                    <span className="text-red-600">Email is required</span>
+                  )}
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type="password"
+                    {...register("password", {
+                      required: true,
+                      minLength: 6,
+                      maxLength: 20,
+                      pattern:
+                        /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
+                    })}
+                    placeholder="Enter Password"
+                    className="input input-bordered"
+                  />
+                  {errors.password?.type === "required" && (
+                    <p className="text-red-600">Password is required</p>
+                  )}
+                  {errors.password?.type === "minLength" && (
+                    <p className="text-red-600">
+                      Password must be 6 characters
+                    </p>
+                  )}
+                  {errors.password?.type === "maxLength" && (
+                    <p className="text-red-600">
+                      Password must be less than 20 characters
+                    </p>
+                  )}
+                  {errors.password?.type === "pattern" && (
+                    <p className="text-red-600">
+                      Password must have one Uppercase one lower case, one
+                      number and one special character.
+                    </p>
+                  )}
+                </div>
+                <div className="form-control mt-6">
+                  <input
+                    className="btn px-2 py-3 w-full bg-[#e39e37b2] hover:bg-[#a97f41b2]  text-lg text-white font-semibold rounded-lg"
+                    type="submit"
+                    value="Sign Up"
+                  />
+                </div>
+              </form>
+              <div className="">
+                <div className="font-semibold text-slate-500 text-center">
+                  Already have an account?
+                  <Link
+                    to="/login"
+                    className="ml-2 text-[#b06a00] hover:underline hover:underline-offset-4"
+                  >
+                    Log In
+                  </Link>
+                </div>
+                <h3 className="text-lg mt-4 mb-1 font-semibold text-center">
+                  Or Sign Up with
+                </h3>
+                <div className="flex justify-center items-center space-x-3">
+                  <button className="p-2 border-2 border-[#b06a00] rounded-full">
+                    <FaFacebookF />
+                  </button>
+                  <button className="p-2 border-2 border-[#b06a00] rounded-full">
+                    <FaGoogle />
+                  </button>
+                  <button className="p-2 border-2 border-[#b06a00] rounded-full">
+                    <FaGithub />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
