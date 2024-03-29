65f136ebd5b9dc474c7ac94ff71e4d26b9165e46
Md Shakil Hossain <mrshakilhossainofficial@gmail.com>
2024-03-30 05:37:10 +0600

    added update user as an admin operation

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -13,7 +13,22 @@ const AllUsers = () => {
     },
   });
 
-  
+  const handleMakeAdmin = (user) => {
+    axiosSecure.patch(`/users/admin/${user._id}`).then((res) => {
+      console.log(res.data);
+      if (res.data.modifiedCount > 0) {
+        refetch();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${user.name} is an Admin Now!`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
+  };
+
 
   return (
     <div>
